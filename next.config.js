6f89e9910ef19ffb57a5ac51c8767fd51ff43d31
jeremytomlinson <jeremy.tomlinson@gmail.com>
2022-09-06 19:49:30 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   basePath: '/azure',
   assetPrefix: '/azure/', // assetPrefix requires the trailing slash
+  publicRuntimeConfig: {
+        basePath: '/azure',
+    },
   eslint: {
     ignoreDuringBuilds: true,
   },
